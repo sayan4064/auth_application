d31c66e7738e8b6adb535486ce8c18d66f861885
sayan <metyasayan3@gmail.com>
2026-08-22 07:01:51 +0530

    Auto backup

--- auth_application/src/main/java/com/auth_application/dtos/RefreshTokenResponse.java
+++ auth_application/src/main/java/com/auth_application/dtos/RefreshTokenResponse.java
@@ -1,5 +1,14 @@
 package com.auth_application.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class RefreshTokenResponse {
     String accessToken;
     String refreshToken;
